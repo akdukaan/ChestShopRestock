791c9a302ec6c5c0b651a5c97be1da6902fc1faf
Kaan <drbot7@gmail.com>
2024-08-06 22:48:34 -0500

    Add drilldown metrics

--- src/main/java/com/birdflop/chestshoprestock/ChestShopRestock.java
+++ src/main/java/com/birdflop/chestshoprestock/ChestShopRestock.java
@@ -1,7 +1,11 @@
 package com.birdflop.chestshoprestock;
 
+import com.google.common.collect.ImmutableMap;
 import org.bstats.bukkit.Metrics;
+import org.bstats.charts.DrilldownPie;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Map;
 
 public final class ChestShopRestock extends JavaPlugin {
 
@@ -21,12 +25,30 @@ public final class ChestShopRestock extends JavaPlugin {
 
         getCommand("restock").setExecutor(new CommandRestock());
         getCommand("csrestock").setExecutor(new CommandCsrestock());
+        try {
+            addCustomMetrics();
+        } catch (Exception ignored) {
 
-        new Metrics(this, 22906);
+        }
+
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    private void addCustomMetrics() {
+        Metrics bStats = new Metrics(this, 22906);
+        String serverVersion = getServer().getBukkitVersion().split("-")[0];
+
+        bStats.addCustomChart(createStaticDrilldownStat("version_mc_plugin", serverVersion, getDescription().getVersion()));
+        bStats.addCustomChart(createStaticDrilldownStat("version_plugin_mc", getDescription().getVersion(), serverVersion));
+
+        bStats.addCustomChart(createStaticDrilldownStat("version_brand_plugin", getServer().getName(), getDescription().getVersion()));
+        bStats.addCustomChart(createStaticDrilldownStat("version_plugin_brand", getDescription().getVersion(), getServer().getName()));
+
+        bStats.addCustomChart(createStaticDrilldownStat("version_mc_brand", serverVersion, getServer().getName()));
+        bStats.addCustomChart(createStaticDrilldownStat("version_brand_mc", getServer().getName(), serverVersion));
+    }
+
+    private static DrilldownPie createStaticDrilldownStat(String statId, String value1, String value2) {
+        final Map<String, Map<String, Integer>> map = ImmutableMap.of(value1, ImmutableMap.of(value2, 1));
+        return new DrilldownPie(statId, () -> map);
     }
 }
